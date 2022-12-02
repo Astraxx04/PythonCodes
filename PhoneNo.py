0fc94bec924dc0b5c7aa31dcc726393556c714b2
Astraxx04 <gagan200254@gmail.com>
2022-12-03 00:19:28 +0530

    Pylance

--- PhoneNo.py
+++ PhoneNo.py
@@ -5,7 +5,7 @@ def isPhoneNumber(text):
     for i in range(0, 3):
        	if not text[i].isdecimal():
        		return False
-    if(text[3] != '-'):
+    if text[3] != '-':
         return False
     for i in range(4, 7):
         if not text[i].isdecimal():
